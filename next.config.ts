b3e4fa97920d0cb4a017c2194b3f29d4e98beebc
cheriff <128246394+Moutail@users.noreply.github.com>
2024-12-25 20:00:11 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,24 +3,10 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    loader: 'default',
-    path: '',
   },
-  assetPrefix: '/Mokpopko_ferme', // Corrigé ici : on utilise un slash au début au lieu de './'
   basePath: '/Mokpopko_ferme',
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(mp4|webm)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next',
-          name: 'static/media/[name].[hash].[ext]',
-        },
-      },
-    });
-    return config;
-  },
+  assetPrefix: '/Mokpopko_ferme/',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
