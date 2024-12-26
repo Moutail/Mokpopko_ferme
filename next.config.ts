12be7f4e05aecc0dbf642d11414dfdc0e5daffdb
cheriff <128246394+Moutail@users.noreply.github.com>
2024-12-25 20:10:49 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,28 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    loader: 'custom',
+    loaderFile: './image-loader.js',
   },
   basePath: '/Mokpopko_ferme',
   assetPrefix: '/Mokpopko_ferme/',
   trailingSlash: true,
+  // Ajout de la configuration des images
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/images/',
+            outputPath: 'static/images/',
+          },
+        },
+      ],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
