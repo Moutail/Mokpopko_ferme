d098cbf60b99e98fba6be248541ad04cdc58af7d
cheriff <128246394+Moutail@users.noreply.github.com>
2024-12-25 22:50:24 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,10 @@
-import type { Configuration } from 'webpack'
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/Mokpopko_ferme',
-  assetPrefix: '/Mokpopko_ferme/',
-  trailingSlash: true,
-  // Ajout de la configuration des images avec typage correct
-  webpack: (config: Configuration) => {
-    config.module?.rules?.push({
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next/static/images/',
-            outputPath: 'static/images/',
-          },
-        },
-      ],
-    })
-    return config
-  },
+  basePath: '/Mokpopko_ferme'
 }
 
-export default nextConfig
+module.exports = nextConfig
