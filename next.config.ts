ef1ac79531aac8a4050abd7a55977005f6fcbc4c
cheriff <128246394+Moutail@users.noreply.github.com>
2024-12-25 19:43:21 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,24 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    loader: 'default',
+    path: '',
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/Mokpopko_ferme' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Mokpopko_ferme/' : '',
+  assetPrefix: './',
+  basePath: '/Mokpopko_ferme',
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next',
+          name: 'static/media/[name].[hash].[ext]',
+        },
+      },
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
